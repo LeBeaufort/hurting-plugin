36dc9d42589cd468ed13a357e726420bccf0f165
LeBeaufort <eloigh@laposte.net>
2024-02-20 12:29:54 +0100

    adding file + self flash case

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <swiftly/swiftly.h>
+#include <swiftly/server.h>
+#include <swiftly/database.h>
+#include <swiftly/commands.h>
+#include <swiftly/configuration.h>
+#include <swiftly/logger.h>
+#include <swiftly/timers.h>
+#include <swiftly/gameevents.h>
+
+Server *server = nullptr;
+PlayerManager *g_playerManager = nullptr;
+Database *db = nullptr;
+Commands *commands = nullptr;
+Configuration *config = nullptr;
+Logger *logger = nullptr;
+Timers *timers = nullptr;
+
+void OnProgramLoad(const char *pluginName, const char *mainFilePath)
+{
+    Swiftly_Setup(pluginName, mainFilePath);
+
+    server = new Server();
+    g_playerManager = new PlayerManager();
+    commands = new Commands(pluginName);
+    config = new Configuration();
+    logger = new Logger(mainFilePath, pluginName);
+    timers = new Timers();
+}
+
+void OnPluginStart()
+{
+    print("Hurting-Plugin started \n");
+}
+
+void OnPluginStop()
+{
+    print("Hurting-plugin stoped");
+}
+
+const char *GetPluginAuthor()
+{
+    return "LeBeaufort";
+}
+
+const char *GetPluginVersion()
+{
+    return "0.0.0";
+}
+
+const char *GetPluginName()
+{
+    return "hurting-plugin";
+}
+
+const char *GetPluginWebsite()
+{
+    return "";
+}
+
+
+//real code start here
+void OnPlayerBlind(Player* player, Player* attacker, short entityid, float duration)
+{
+    //first, we check if the player self-flashed
+    if (player == attacker && duration > 0.5)
+    {
+        player->SendMsg(HudDestination(4), "ha, you so good, you flashed yourself");
+        print("ha, you so good, you flashed yourself");
+    }
+}
