1c4a9d4adfa9849aee8cb22b7b9a30a4ed451c1f
LeBeaufort <eloigh@laposte.net>
2024-03-13 15:27:34 +0100

    fixing linking error

--- src/playereventsmanager.cpp
+++ src/playereventsmanager.cpp
@@ -4,84 +4,69 @@
 
 #include <swiftly/swiftly.h>
 
+#include "playereventsmanager.hpp"
 
 
-class PlayerEvent
-{
-    public:
-        int time;
-        std::string type;
-        Player* player;
-        
-        PlayerEvent(std::string type, Player* player)
-        {
-            this->time = std::time(0);
-            this->type = type;
-            this->player = player;
-        }
+
 
         
-};
-
-
-
-
-
-class PlayerEventsManager
+PlayerEvent::PlayerEvent(std::string type, Player* player)
 {
+        this->time = std::time(0);
+        this->type = type;
+        this->player = player;
+}
 
-public:
 
-    PlayerEventsManager(){}
 
-    void add_event(std::string type, Player* player)
+PlayerEventsManager::PlayerEventsManager(){}
+
+void PlayerEventsManager::add_event(std::string type, Player* player)
+{
+    //before adding anything, we check if it exist.
+    int index = this->getIndexOf(type, player);
+
+    if (index != -1)
     {
-        //before adding anything, we check if it exist.
-        int index = this->getIndexOf(type, player);
+        //we change the time
+        this->events[index].time = std::time(0);
+    }
+    else
+    {
+        // if it does exist yet, we create it
+        this->events.push_back(PlayerEvent(type, player));
+    }
+}
 
-        if (index != -1)
+int PlayerEventsManager::getTimeithappen(std::string type, Player* player)
+{
+    int index = this->getIndexOf(type, player);
+    if (index != -1)
+    {
+        return this->events[index].time;
+    }
+    else {
+        return 0;
+    }
+}
+
+void PlayerEventsManager::clear()
+{
+    this->events.clear();
+}
+
+
+int PlayerEventsManager::getIndexOf(std::string type, Player* player)
+{
+    /*this function search for an PlayerEvent that match with the type and the player. It returns the index of this PlayerEvent*/
+    for (int i = 0; i++; i < this->events.size())
+    {
+        if (type == this->events[i].type && player == events[i].player)
         {
-            //we change the time
-            this->events[index].time = std::time(0);
-        }
-        else
-        {
-            // if it does exist yet, we create it
-            this->events.push_back(PlayerEvent(type, player));
+            return i;
         }
     }
-
-    int getTimeithappen(std::string type, Player* player)
-    {
-        int index = this->getIndexOf(type, player);
-        if (index != -1)
-        {
-            return this->events[index].time;
-        }
-    }
-
-    void clear()
-    {
-        this->events.clear();
-    }
-
-
-private:
-
-    std::vector<PlayerEvent> events;
-
-    int getIndexOf(std::string type, Player* player)
-    {
-        /*this function search for an PlayerEvent that match with the type and the player. It returns the index of this PlayerEvent*/
-        for (int i = 0; i++; i < this->events.size())
-        {
-            if (type == events[i].type && player == events[i].player)
-            {
-                return i;
-            }
-        }
-        // if nothing is found -1 is returned
-        return -1;
-    }
-};
+    // if nothing is found -1 is returned
+    return -1;
+}
 
